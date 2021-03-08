f3324e16c42a5b2b5acad3d1b060d9322f5773b0
Paul Schuladen <paul.schuladen@posteo.de>
2021-03-08 19:53:02 +0100

    improved channel fetching behaviour

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ The tools in this repository are meant as supplement for RMEs Totalmix for contr
 
 Basic functions for toggling mute, loopback etc. were implemented. Future features will include functions for setting diagonal routing or other volume configurations and creating fadergroups via snapshot files.
 
-In order to work properly the tool has to fetch the complete channel layout and optionally all channel gains. The fetched data is written into a json-file and must only refetched if the layout changes. (un-/hiding channels, change channel names or change stereo settings). A layer must not contain two or more channel with the same name. This might lead to confusing behavior because the channelnames were also used as orientation.
+In order to work properly the tool has to fetch the complete channel layout and channel gains. For that all channels have to be visible for the controller. So unhide all in the channel layout menu of totalmix.
+
+The fetched data is written into a json-file and must only refetched if the layout changes. (un-/hiding channels, change channel names or change stereo settings). A layer must not contain two or more channel with the same name. This might lead to confusing behavior because the channelnames were also used as orientation.
--- src/ps-totalmixCli.py
+++ src/ps-totalmixCli.py
@@ -14,8 +14,8 @@ import time, datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interactive', action='count', help='run in interactive mode')
-parser.add_argument('-f', '--fetch', action='store_true', help='fetch all device properties')
-parser.add_argument('-F', '--file', default='tmChProps.json', help='use prefetched values from file or fetch and write to file if -f')
+parser.add_argument('-f', '--fetch', action='count', help='fetch all device properties')
+parser.add_argument('-F', '--file', default='tm-fetched.json', help='use prefetched values from file or fetch and write to file if -f')
 parser.add_argument('-o', '--port', default=2, help='either 1,2,3 or 4, default is 2 for second default osc-control (9002:localhost:7002')
 parser.add_argument('-r', '--remote', help='<portTmOut>:<tm-ip>:<tmReceive>, e.g. 9002:192.168.178.27:7002 for local control for default osc-control 2'
                                            ' overwrites -c')
@@ -25,7 +25,7 @@ parser.add_argument('-a', '--action', default='set', help='action to perform. se
 parser.add_argument('-p', '--parameter', default='', help='parameter to set.')
 parser.add_argument('-vf', '--value', type=float, default=0, help='number-value to set to from 0 to 1')
 parser.add_argument('-val' '--realvalue', default='', help='real vlaue e.g. "300hz" or "-6db"')
-parser.add_argument('-ch', '--channel', default='', help='channel to process starting with 1, e.g. "output:[1:3],6,[13:16]", "playback:1,2,3,4,5", "input:[5:10]"')
+parser.add_argument('-ch', '--channel', default='', help='channel to process starting with 1, e.g. "output.1:3,6,13:16", "playback:1,2,3,4,5", "input:[5:10]"')
 
 
 
@@ -61,17 +61,13 @@ def oscR_selectedSubmix(mode: int, *args):
     selectedSubmixName = args[0].decode()
 
     if channelPropertiesFetched:
-        # print('eindeutiger identifier', len(channelNamesByIndex[output]))
         selectedSubmixIndex = channelNamesByIndex[output].index(selectedSubmixName)
 
     if mode == 2:
-        tmOutputComplete()
+        checkTaskStack()
     elif mode == 1:
         global countFader
         if countFader:
-            # sOsc = '/1/MicGainVal{}'.format(numberFader)
-            # oscServer.bind(sOSc.encode(), tmOutputComplete)
-
             countFader = False
 
 
@@ -100,25 +96,23 @@ def oscR_countRemoteFader(nFader, *args):
 
 
 def oscR_tmpChannelDataMode1(fader:int, key:str, *args):
-    # if type(args[0]) == type(bytes):
-    #     value = args[0].decode()
-    # else:
-    #     value = args[0]
+
     try:
         value = args[0].decode()
     except:
         value = args[0]
 
-    # if not key in tmpChannelDataMode1.keys():
-    #     tmpChannelDataMode1[key] = {}
     tmpChannelDataMode1[key][fader] = value
+
     if fader == 1 and key == 'name':
+        tmpChannelName = value
         global currentChIndex
-        currentChIndex = channelNamesByIndex[currentLayer].index(value)
+        if value in channelNamesByIndex:
+            currentChIndex = channelNamesByIndex[currentLayer].index(value)
 
 
 def oscR_dataMode1complete(*args):
-    tmOutputComplete()
+    checkTaskStack()
 
 
 def createChanVolDic(vol=0.0, pan=0.5) -> dict:
@@ -128,15 +122,30 @@ def createChanVolDic(vol=0.0, pan=0.5) -> dict:
     }
 
 
-def tmOutputComplete(*args):
-    try:
-        doWhatYouMustDo.pop(0)()
-    except:
+def checkTaskStack(*args):
+    global timeout
+    timeout.cancel()
+
+    if taskStack:
+        timeout = scheduleTimeOut(taskStack[0])
+        timeout.start()
+        taskStack.pop(0)()
+    else:
         print('no tasks on stack')
+        timeout = scheduleTimeOut()
+        timeout.start()
+
+    # try:
+    #     # print(taskStack[0])
+    #
+    #     taskStack.pop(0)()
+    # except:
+    #     print('no tasks on stack')
 
 
 
 def oscS_goToLayer(layer: str, mode: int = 2):
+    initTmpData()
     sOsc = '/{}/{}'.format(mode, layer)
     toTM.send_message(sOsc.encode(), [1.0])
 
@@ -156,111 +165,78 @@ def oscS_goToNextChannel():
 
     toTM.send_message(b'/2/track+', [1.0])
 
+def oscS_previousChannel(mode:int=2):
+    if mode == 2:
+        toTM.send_message(b'/2/track-', [1.0])
+    else:
+        toTM.send_message(b'/1/track+', [1.0])
+
 def oscS_goToNextBank():
     toTM.send_message(b'/1/bank+', [1.0])
 
+def oscS_previousBank():
+    toTM.send_message(b'/1/bank-', [1.0])
+
+
 def oscS_selectSubmix(outChannel:int):
-    toTM.send_message(b'/setSubmix', [float(outChannel)])
+    if not selectedSubmixIndex == outChannel:
+        toTM.send_message(b'/setSubmix', [float(outChannel)])
+        global timeout
+        if timeout.is_alive():
+            timeout.cancel()
+        timeout = threading.Timer(0.5, checkTaskStack)
+        timeout.start()
+    else:
+        checkTaskStack()
 
 
-# def goToChannelName(name: str):
-#     pass
-
+lastChName = None
+def goToFirstChannel(mode:int=1):
+    if tmpChannelName == lastChName:
+        checkTaskStack()
+    else:
+        taskStack.insert(0, partial(goToFirstChannel, mode))
+        oscS_previousBank()
 
 def getDataOfSelectedChannel() -> dict:
     return channelDataByName[currentLayer][channelNamesByIndex[currentLayer][currentChIndex]]
 
 def getChannelDataByIndex(layer:str, idx:int, key:str=''):
-    # print('getting channel data ', layer, idx, key)
     if key:
         return channelDataByName[layer][channelNamesByIndex[layer][idx]][key]
     else:
         return channelDataByName[layer][channelNamesByIndex[idx]]
-
-def fetchDeviceState(fetchtask):
-
-    doWhatYouMustDo.append(fetchtask)
-
-    mode = 2 if fetchtask == fetchLayerPorperties else 1
-
-    if currentLayer == '':
-        oscS_goToLayer(output, mode)
-    elif currentLayer == output:
-        oscS_goToLayer(input, mode)
-    elif currentLayer == input:
-        oscS_goToLayer(playback, mode)
-    elif currentLayer == playback:
-        global channelVolumeFetched
-
-        if fetchtask == fetchLayerPorperties:
-            global channelPropertiesFetched, numberTmChannels
-            numberTmChannels = len(channelNamesByIndex[input])
-            doWhatYouMustDo.pop()
-            channelPropertiesFetched = True
-
-        elif fetchtask == fetchLayerVolume:
-            global channelVolumeFetched
-
-            doWhatYouMustDo.pop()
-            channelVolumeFetched = True
-
-
-
-        startInit()
-    else:
-        finishInit()
-
-
-def fetchLayerPorperties():
-
-    global currentChIndex, checkChannelLimit, chLimitReached
-    currentChIndex = 0
-    channelNamesByIndex[currentLayer] = []
-    channelDataByName[currentLayer] = {}
-    doWhatYouMustDo.append(fetchChannelProperties)
-    checkChannelLimit = False
-    chLimitReached = False
-    oscS_goToChannelIndex(0)
 
 
 def fetchChannelProperties():
 
     global chLimitReached
     if chLimitReached:
-        fetchDeviceState(fetchLayerPorperties)
+        chLimitReached = False
+        checkTaskStack()
     else:
         global tmpChannelData, checkChannelLimit
-
         channelNamesByIndex[currentLayer].append(tmpChannelName)
         if tmpChannelData['stereo'] == 1.0:
             channelNamesByIndex[currentLayer].append(tmpChannelName)
         channelDataByName[currentLayer][tmpChannelName] = tmpChannelData.copy()
         tmpChannelData = {}
         checkChannelLimit = True
-        doWhatYouMustDo.append(fetchChannelProperties)
+        taskStack.insert(0, fetchChannelProperties)
 
         oscS_goToNextChannel()
 
 
-# def fetchDeviceVolume():
-#     pass
-
-def fetchLayerVolume():
-    if selectedSubmixIndex > 0:
-        doWhatYouMustDo.append(fetchLayerVolume)
-        oscS_selectSubmix(0)
-
-    else:
-        doWhatYouMustDo.append(fetchChannelVolume)
-        oscS_goToChannelIndex(0)
-
-
 def fetchChannelVolume():
-    global tmpChannelDataMode1
-    for idx in range(len(tmpChannelDataMode1['name'])):
+    global tmpChannelDataMode1, channelSends, outputReceives, outputVolumes
+    countedFader = numberFader
+    for idx in range(len(tmpChannelDataMode1['name'].keys())):
         chName = tmpChannelDataMode1['name'][idx]
-        chIdx = channelNamesByIndex[currentLayer].index(chName)
+        if chName == 'n.a.':
+            countedFader = idx
+            break
 
+        chIdx = channelNamesByIndex[currentLayer].index(chName)
         chDic = createChanVolDic(tmpChannelDataMode1['vol'][idx],
                                  tmpChannelDataMode1['pan'][idx])
         if currentLayer == output:
@@ -277,10 +253,13 @@ def fetchChannelVolume():
             channelSends[currentLayer][chIdx][selectedSubmixIndex] = chDic
             outputReceives[currentLayer][outputIndex][chIdx] = chDic
 
-    lastChannelReached = bool(tmpChannelDataMode1['name'][numberFader-1] == channelNamesByIndex[currentLayer][-1])
+    lastChannelReached = bool(tmpChannelDataMode1['name'][countedFader-1] == channelNamesByIndex[currentLayer][-1])
     tmpChannelDataMode1 = initTmpChannelDataMode1()
-
     if lastChannelReached:
+        # if currentLayer == output:
+        #     checkTaskStack()
+        #     return
+
         if channelNamesByIndex[output][selectedSubmixIndex + 1] == channelNamesByIndex[output][selectedSubmixIndex]:
             nextSubmixIdx = selectedSubmixIndex + 2
         else:
@@ -288,45 +267,67 @@ def fetchChannelVolume():
 
 
         if nextSubmixIdx < numberTmChannels:
-            doWhatYouMustDo.append(partial(oscS_goToChannelIndex, 0))
-            doWhatYouMustDo.append(fetchChannelVolume)
+            taskStack.insert(0, fetchChannelVolume)
+            taskStack.insert(0, partial(oscS_goToChannelIndex, 0))
             oscS_selectSubmix(nextSubmixIdx)
         else:
-            fetchDeviceState(fetchLayerVolume)
+            checkTaskStack()
 
     else:
-        doWhatYouMustDo.append(fetchChannelVolume)
+        taskStack.insert(0, fetchChannelVolume)
         oscS_goToNextBank()
 
+def setPropertiesFetched(val:bool=True):
+    global channelPropertiesFetched
+    channelPropertiesFetched = val
+    global numberTmChannels
+    numberTmChannels = len(channelNamesByIndex[input])
+    checkTaskStack()
+
+def setVolumesFetched(val:bool=True):
+    global channelVolumeFetched
+    channelVolumeFetched = val
+    checkTaskStack()
 
 def startInit():
 
     if not shutdown:
-
+        thingsToFetch = []
+        if shallFetchProperties:
+            print('fetch properties')
+            thingsToFetch.append((fetchChannelProperties, 2))
+        if shallFetchVolumes:
+            print('fetch gains')
+            thingsToFetch.append((fetchChannelVolume, 1))
         global currentLayer
-        if shallFetchProperties and not channelPropertiesFetched:
-            currentLayer = ''
-            print('fetching channel properties')
-            fetchDeviceState(fetchLayerPorperties)
-            return
-        elif shallFetchVolumes and not channelVolumeFetched:
-            currentLayer = ''
-            print('fetching channel gains')
-            fetchDeviceState(fetchLayerVolume)
-            return
+        currentLayer = ''
 
-        finishInit()
+        for fetch, mode in thingsToFetch:
+            for layer in [output,input,playback]:
+                print('setting up tasks', fetch, mode, layer)
+                taskStack.append(partial(oscS_goToLayer, layer, mode))
+                if mode == 1:
+                    taskStack.append(partial(oscS_selectSubmix, 0))
+                taskStack.append(partial(oscS_goToChannelIndex, 0))
+                taskStack.append(fetch)
+            if mode == 2:
+                taskStack.append(setPropertiesFetched)
+            elif mode == 1:
+                taskStack.append(setVolumesFetched)
+
+
+        taskStack.append(finishInit)
+        checkTaskStack()
 
 
 shutdown = False
 def finishInit():
 
     print('finished init')
-
-    global numberTmChannels
-    numberTmChannels = len(channelNamesByIndex[input])
+    print('opentasks:', taskStack)
 
     if args.fetch and args.file:
+        # print('filename', args.file)
         fname = args.file
         with open(fname, 'w') as fp:
             completeDict = {
@@ -344,8 +345,10 @@ def finishInit():
 
 
     verifyTargetChannels()
-
-    print(' start setting parameter', targetParameter, targetValue, 'for channels', channelsToSet, 'on layer', layerToSet)
+    if targetParameter:
+        print(' start setting parameter', targetParameter, targetValue, 'for channels', channelsToSet, 'on layer', layerToSet)
+    else:
+        print('no parameter to set')
 
     if channelsToSet:
         prepareSettingValues()
@@ -354,7 +357,6 @@ def finishInit():
 
 
 def verifyTargetChannels():
-    # print('verify target channels', channelNameRanges, numberTmChannels)
 
     while (channelNameRanges):
         chRange = channelNameRanges.pop()
@@ -365,8 +367,7 @@ def verifyTargetChannels():
             try:
                 lowIdx = channelNamesByIndex[layerToSet].index(chRange[0])
                 highIdx = channelNamesByIndex[layerToSet].index(chRange[1])
-                # if lowIdx > 0 and lowIdx % 2:
-                #     lowIdx = lowIdx - 1 if channelDataByName[layerToSet]
+
                 for c in range(lowIdx, highIdx + 1):
                     channelsToSet.add(c)
             except:
@@ -380,13 +381,10 @@ def verifyTargetChannels():
             chSt = bool(getChannelDataByIndex(layerToSet, c, 'stereo'))
             if chSt:
                 _channelsToRemove.add(c)
-                # channelsToSet.remove(c)
                 _channelsToAdd.add(c - 1)
-                # channelsToSet.add(c - 1)
 
         if c >= numberTmChannels:
             _channelsToRemove.add(c)
-            # channelsToSet.remove(c)
             _limitReached = True
     for c in _channelsToRemove:
         channelsToSet.remove(c)
@@ -400,8 +398,8 @@ def verifyTargetChannels():
 def prepareSettingValues():
 
 
-    doWhatYouMustDo.clear()
-    doWhatYouMustDo.append(setValuesForTargetChannels)
+    taskStack.clear()
+    taskStack.append(setValuesForTargetChannels)
 
     oscS_goToLayer(layerToSet)
 
@@ -413,7 +411,6 @@ def prepareSettingValues():
 settingTargetValue = False
 def checkValue():
     global settingTargetValue
-    # print('checkingvalue', currentLayer, currentChIndex, tmpChannelData[targetParameter], targetValue)
     if tmpChannelData[targetParameter] == targetValue:
         settingTargetValue = False
         setValuesForTargetChannels()
@@ -426,7 +423,6 @@ def checkValue():
 
 
 def oscS_doSetValue(oscAd, value):
-    # print('setting value')
     toTM.send_message(oscAd, [value])
 
 def oscS_toggleValue(oscAd):
@@ -436,16 +432,12 @@ def oscS_toggleValue(oscAd):
 def setValuesForTargetChannels():
     if channelsToSet:
         c = channelsToSet.pop()
-        doWhatYouMustDo.append(checkValue)
+        taskStack.append(checkValue)
         oscS_goToChannelIndex(c)
 
     else:
         print('finished setting values')
         exitProgram()
-
-
-
-    # exitProgram()
 
 
 def exitProgram():
@@ -458,12 +450,27 @@ def exitProgram():
 
 def sshhhuh():
     print('ssshshhhuuuu', os.getpid())
+    timeout.cancel()
+    # del timeout
+    oscServer.close()
+    # del oscServer
     os.kill(os.getpid(), signal.SIGQUIT)
+
+
+def scheduleTimeOut(lastFunction=None, t=5):
+    return threading.Timer(t, timeoutCalled, args=[lastFunction])
+
+
+def timeoutCalled(lastFunctionCall=None):
+    print('process timeout.\nTotalmix is not responding, configuration has failures or there is a bug.'
+          '\nLast function called is', lastFunctionCall, '\nTasks on stack are', taskStack)
+    exitProgram()
 
 
 def dumdumdummyfunc():
     pass
 
+timeout = scheduleTimeOut()
 
 shallFetchProperties = True
 channelPropertiesFetched = False
@@ -501,6 +508,12 @@ def initTmpChannelDataMode1() -> dict:
 
 tmpChannelDataMode1 = initTmpChannelDataMode1()
 
+def initTmpData():
+    global tmpChannelDataMode1, tmpChannelData, tmpChannelName
+    tmpChannelData = {}
+    tmpChannelName = ''
+    tmpChannelDataMode1 = initTmpChannelDataMode1()
+
 channelNamesByIndex = {
     input: [],
     playback: [],
@@ -524,9 +537,7 @@ outputReceives = {
 }
 outputVolumes = {}
 
-doWhatYouMustDo = []
-
-
+taskStack = []
 
 
 # parsing input
@@ -536,9 +547,9 @@ if args.interactive:
 
 
 fetchtime = 'fetchtime'
-if args.fetch:
-    shallFetchVolumes = True
-    shallFetchProperties = True
+if args.fetch and args.fetch > 0:
+    shallFetchProperties = args.fetch > 0
+    shallFetchVolumes = args.fetch > 1
 else:
     print('dont fetch channel layout')
     shallFetchVolumes = False
@@ -583,17 +594,22 @@ channelsToSet = set()
 channelNameRanges = set()
 layerToSet = ''
 if args.channel:
-    _layer = ''
-    splitIdx = 0
+    channelArgs = args.channel.split('.')
+    _layer = channelArgs[0]
+    # splitIdx = args.channel.find('.')
+    # if splitIdx > 0:
+    #     _layer = args.channel[]
+
     # print(args.channel)
-    try:
-        splitIdx = args.channel.index(':')
-        # print(splitIdx)
-        _layer = args.channel[:splitIdx]
-    except:
-        print('ERROR something wrong with layer selection')
+
+    # try:
+    #     splitIdx = args.channel.index('.')
+    #     # print(splitIdx)
+    #     _layer = args.channel[:splitIdx]
+    # except:
+    #     print('ERROR something wrong with layer selection')
     if _layer in ['input', 'output', 'playback']:
-        #TODO: dummes Konstrukt
+        #TODO: dummes Konstrukt wegmachen
         _layerNames = {
             'input': input,
             'output': output,
@@ -601,19 +617,19 @@ if args.channel:
         }
         layerToSet =  _layerNames[_layer]
 
-        _channelsRaw = args.channel[splitIdx+1:]
-
-
-        if _channelsRaw in ['', 'all', ':']:
+        if len(channelArgs)==1 or channelArgs[0] in ['', 'all', ':']:
             channelNameRanges.add('all')
-            # print('target are all channels of layer', _layer)
+
         else:
-            try:
-                _channelsRaw = _channelsRaw.split(',')
-            except:
-                _channelsRaw = [_channelsRaw]
+            _channelsRaw = channelArgs[1].split(',')
+
+            # try:
+            #     _channelsRaw = _channelsRaw.split(',')
+            # except:
+            #     _channelsRaw = [_channelsRaw]
 
             _channelStrings = set()
+
             for _ch in _channelsRaw:
 
                 try:
@@ -679,11 +695,9 @@ for i in range(64):
 
 oscServer.bind(b'/1/micgain8', oscR_dataMode1complete)
 
-
 if shutdown:
     print('shutting down... cause I got nothing to do.. or something went wrong')
     exitProgram()
 else:
     startInit()
     signal.pause()
-# signal.pause()
